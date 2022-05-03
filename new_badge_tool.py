9ca076b423856bb93980995834d52a8ef7d4b19e
Dustin Heywood <dustin.heywood@gmail.com>
2022-05-02 23:40:33 -0600

    Update new_badge_tool.py

--- new_badge_tool.py
+++ new_badge_tool.py
@@ -1,4 +1,7 @@
 # LqqkOut's code for badge transmissions
+# carfucar checksum generator
+# evilmog heavily modified and rewrote the vendo() and badge send
+# MIT Licensed
 
 import serial
 import time
@@ -10,58 +13,64 @@ ser = serial.Serial(ser_device, 4800, timeout=2)
 ser.flushInput()
 ser.flushOutput()
 
-# give_points(): broadcast a badge ID, this is to simulate "meeting" other badges 
-# around the con and was helpful for racking up points for the vending machine.
-# only ~700 points could be gained by iterating through badge id's so there's
-# some other secret sauce (status byte from doing the quests?) that would 
-# hopefully convince the vending machine that you had a high enough balance
-# to buy the black badge. 
-def give_points(statusbyte, start, end, time_delay): 
-    intro=bytearray.fromhex("536D6173683F"+statusbyte) # 01
-    #for x in range (0,65536):
-    #for x in range (0,67000):
-    for x in range (start,end):
-        mystring=hex(x)
-        padded = str.format('{:06X}', int(mystring, 16))
-        output = intro+bytearray.fromhex(padded)
-        check_byte = 0xFF - (sum(output)-1)%256
-        output.append(check_byte)
-        print("X: ",x," Payload: ",output)
-        ser.write(output)
-        time.sleep(time_delay)
-
 # Render bytes from the received serial data
 def pretty_bytes(intro, data_raw, first_byte, last_byte):
     bytestring = (''.join(r' '+hex(letter)[2:] for letter in data_raw[first_byte : last_byte]))
     print(intro + ": " + bytestring)
 
 #pretend to be a vending machine - send a code, badge dumps status back
+# new parser based on https://github.com/Wireb/Cyphercon-5_3-2022/blob/master/perl/Vendo_test.pl
 def vendo(): 
     vendo=bytearray.fromhex("536D6173683F000102ffc3")
+    ser.flushInput()
     ser.write(vendo)
     data_raw=ser.readline()
-    data_array = data_raw.decode("all-escapes").split("\\x")
+    data_array = data_raw.decode("all-escapes").replace("\\x", "")
+    expected_header = '536d6173683f' # Smash?
 
-    print(data_array)
-    badge_id = data_array[20] + data_array[21]
-    badge_status = data_array[27]
-    badge_spent = data_array[23] + data_array[24]
-    badge_credits = data_array[32] + data_array[33]
-    print("Badge ID:" + badge_id)
-    print("Badge Status Flag: " + badge_status)
-    print("Spent Credits: " + badge_spent)
-    #print("Remaining Credits: " + badge_credits)
-    # The previous line is just the beginning of a parser that I didn't
-    # have time to complete. Best case is to find the intro string "Smash?" 
-    # and start decoding with the bytes after that. pretty_bytes should take 
-    # an offset and return a nicely-printed representation of those bits to 
-    # match @wireengineer 's screenshots on twitter
-    
-#        print("Checksum:" + data_raw[0].decode("ascii"))
-#        print("Checksum:" + data_raw[1])
-#        print("Checksum:" + data_raw[2])
-#        print("Checksum:" + data_raw[3])
-#        print("Checksum:" + data_raw[4:6])
+    # definitions for decoder
+    if data_array[0:12] == "536d6173683f":
+        print("[+] Correct Header")
+        print("")
+    else:
+        exit()
+
+    # Status
+    badge_status = data_array[14:16]
+    if badge_status == "01":
+        print("[+] Con Start: " + badge_status)
+    if badge_status == "02":
+        print("[-] Sick: " + badge_status)
+
+    # Ping Type
+    badge_ping_type = data_array[16:18]
+    if badge_ping_type == "02":
+        print("[+] Data Dump")
+    else:
+        print("[-] Unknown Packet Type")
+
+  
+# Checksum calculator from carfucar
+def write_this(b_id: int) -> bytes:
+    header = b'Smash?'
+    badge_id = 0
+    header_checksum = 571
+    f1 = (b_id >> 16) & 0xFF
+    #print(f"f1: {f1:x}")
+    f2 = (b_id >> 8) & 0xFF
+    #print(f"f2: {f2:x}")
+    f3 = (b_id >> 0) & 0xFF
+    #print(f"f3: {f3:x}")
+    b_ = b_id.to_bytes(3, 'big')
+    cs = (header_checksum + f1 + f2 + f3) & 0xFF
+    cs += 1
+    cs = (~cs) & 0xFF
+    cs = abs(cs)
+    print(cs)
+    cs += 1
+    cs = (cs) & 0xFF
+    cs_b = cs.to_bytes(1, 'big')
+    return header + b'\x01' + b_ + cs_b
 
 # loop to watch for badge transmissions, also used this to grab the
 # vending machine handshake, other badge types, outhouses, and quest items
@@ -93,24 +102,5 @@ def reset_badge():
     reset_badge_evil=bytearray.fromhex("536D6173683F00DEADBEEF29D6A2EC")
     ser.write(reset_badge_evil)
 
-#vendo()
 
-def find_byte():
-    data_array = bytearray.fromhex("536D6173683F000102FFC3536D6173683F0102020D0702DC013B02FF00024200240E002E71004F4D00309A000201BC016A000F0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFBF080309C80E94").decode("all-escapes").split("\\x")
-    print(data_array)
-    counter = 0
-    for line in data_array:
-        if line == "24":
-            print("Data found: " + str(counter))
-            print("data: " + data_array[counter - 1] + data_array[counter])
-        counter = counter + 1
-    emulated_string = data_array[38]
-    #print(emulated_string)
-
-#find_byte()
-
-#vendo()
-
-#reset_badge()
 vendo()
-
